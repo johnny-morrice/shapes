f18f6bb2dc1ab3cce1db7a7da02a8e02c9d6d434
John Morrice <john@functorama.com>
2017-10-07 12:03:08 +0100

    Define runtime tests

--- shapes_test.go
+++ shapes_test.go
@@ -6,10 +6,233 @@ import (
 	"testing"
 )
 
+func TestRuntimeExecute(t *testing.T) {
+	addInput, addExpect := addTestData()
+	subInput, subExpect := subTestData()
+	pushInput, pushExpect := pushTestData()
+	popInput, popExpect := popTestData()
+	readInput, readExpect := readTestData()
+	writeInput, writeExpect := writeTestData()
+	copyInput, copyExpect := copyTestData()
+	setInput, setExpect := setTestData()
+	jmpnzInput, jmpnzExpect := jmpnzTestData()
+
+	table := [][2]cannedProcess{
+		[2]cannedProcess{addInput, addExpect},
+		[2]cannedProcess{subInput, subExpect},
+		[2]cannedProcess{pushInput, pushExpect},
+		[2]cannedProcess{popInput, popExpect},
+		[2]cannedProcess{readInput, readExpect},
+		[2]cannedProcess{writeInput, writeExpect},
+		[2]cannedProcess{copyInput, copyExpect},
+		[2]cannedProcess{setInput, setExpect},
+		[2]cannedProcess{jmpnzInput, jmpnzExpect},
+	}
+
+	for _, test := range table {
+		runtimeExecuteHelper(t, test[0], test[1])
+	}
+}
+
+func TestOperationLongAddress(t *testing.T) {
+	op := Operation{}
+	op.Operand[0] = Operand(binary("01101010", 8))
+	op.Operand[1] = Operand(binary("11000101", 8))
+	expected := LongAddress(binary("0110101011000101", 16))
+	long := op.LongAddress()
+
+	if long != expected {
+		t.Errorf("Expected %b but received %b", expected, long)
+	}
+}
+
+func jmpnzTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{0, 10}},
+		Operation{OpCode: OP_SET, Operand: [2]Operand{1, 1}},
+		Operation{OpCode: OP_ADD, Operand: [2]Operand{2, 0}},
+		Operation{OpCode: OP_SUB, Operand: [2]Operand{0, 1}},
+		Operation{OpCode: OP_JMPNZ, Operand: [2]Operand{0, 2}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[1] = 55
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, []byte{})
+
+	return input, expect
+}
+
+func addTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{0, 10}},
+		Operation{OpCode: OP_SET, Operand: [2]Operand{1, 20}},
+		Operation{OpCode: OP_ADD, Operand: [2]Operand{0, 1}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[0] = 30
+	register[1] = 20
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, []byte{})
+
+	return input, expect
+}
+
+func subTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{0, 10}},
+		Operation{OpCode: OP_SET, Operand: [2]Operand{1, 20}},
+		Operation{OpCode: OP_SUB, Operand: [2]Operand{1, 0}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[0] = 10
+	register[1] = 10
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, []byte{})
+
+	return input, expect
+}
+
+func pushTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{1, 20}},
+		Operation{OpCode: OP_PUSH, Operand: [2]Operand{0, 1}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[1] = 20
+	stack := [MAX_WORD][]byte{}
+	stack[0] = []byte{20}
+	expect := makeExpectProcess(byteCode, register, stack, []byte{})
+
+	return input, expect
+}
+
+func popTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{0, 40}},
+		Operation{OpCode: OP_SET, Operand: [2]Operand{1, 20}},
+		Operation{OpCode: OP_PUSH, Operand: [2]Operand{0, 0}},
+		Operation{OpCode: OP_PUSH, Operand: [2]Operand{0, 1}},
+		Operation{OpCode: OP_POP, Operand: [2]Operand{0, 2}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[0] = 40
+	register[1] = 20
+	register[2] = 20
+	stack := [MAX_WORD][]byte{}
+	stack[0] = []byte{40}
+	expect := makeExpectProcess(byteCode, register, stack, []byte{})
+
+	return input, expect
+}
+
+func readTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_READ, Operand: [2]Operand{60, 0}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{42})
+
+	register := [MAX_WORD]byte{}
+	register[60] = 42
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, []byte{})
+
+	return input, expect
+}
+
+func writeTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{50, 10}},
+		Operation{OpCode: OP_WRITE, Operand: [2]Operand{50, 0}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[50] = 10
+	output := []byte{10}
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, output)
+
+	return input, expect
+}
+
+func copyTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{0, 10}},
+		Operation{OpCode: OP_COPY, Operand: [2]Operand{1, 0}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[0] = 10
+	register[1] = 10
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, []byte{})
+
+	return input, expect
+}
+
+func setTestData() (cannedProcess, cannedProcess) {
+	byteCode := []Operation{
+		Operation{OpCode: OP_SET, Operand: [2]Operand{0, 10}},
+	}
+
+	input := makeInputProcess(byteCode, []byte{})
+
+	register := [MAX_WORD]byte{}
+	register[0] = 10
+	expect := makeExpectProcess(byteCode, register, [MAX_WORD][]byte{}, []byte{})
+
+	return input, expect
+}
+
+func binary(text string, size int) uint64 {
+	num, err := strconv.ParseUint(text, 2, size)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return num
+}
+
 type cannedProcess struct {
 	process *Process
 	input   *bytes.Buffer
 	output  *bytes.Buffer
+}
+
+func makeInputProcess(byteCode []Operation, input []byte) cannedProcess {
+	return cannedProcess{
+		process: &Process{
+			ByteCode: byteCode,
+		},
+		input:  bytes.NewBuffer(input),
+		output: &bytes.Buffer{},
+	}
+}
+
+func makeExpectProcess(byteCode []Operation, register [MAX_WORD]byte, stack [MAX_WORD][]byte, output []byte) cannedProcess {
+	return cannedProcess{
+		process: &Process{
+			ByteCode: byteCode,
+			Register: register,
+			Stack:    stack,
+		},
+		input:  &bytes.Buffer{},
+		output: bytes.NewBuffer(output),
+	}
 }
 
 func (canned cannedProcess) makeRuntime() *Runtime {
@@ -79,7 +302,7 @@ func (canned cannedProcess) sameOutput(t *testing.T, other cannedProcess) bool {
 	return true
 }
 
-func executeHelper(t *testing.T, input cannedProcess, expect cannedProcess) {
+func runtimeExecuteHelper(t *testing.T, input cannedProcess, expect cannedProcess) {
 	t.Helper()
 
 	runtime := input.makeRuntime()
@@ -98,26 +321,4 @@ func executeHelper(t *testing.T, input cannedProcess, expect cannedProcess) {
 	if !ok {
 		t.Fail()
 	}
-}
-
-func TestOperationLongAddress(t *testing.T) {
-	op := Operation{}
-	op.Operand[0] = Operand(binary("01101010", 8))
-	op.Operand[1] = Operand(binary("11000101", 8))
-	expected := LongAddress(binary("0110101011000101", 16))
-	long := op.LongAddress()
-
-	if long != expected {
-		t.Errorf("Expected %b but received %b", expected, long)
-	}
-}
-
-func binary(text string, size int) uint64 {
-	num, err := strconv.ParseUint(text, 2, size)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return num
 }
