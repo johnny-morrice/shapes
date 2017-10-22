bf08698a3b06ca8b1780fd94343b7c3f8b4abed9
John Morrice <john@functorama.com>
2017-10-22 11:02:41 +0100

    Implement compile visitor for runtime as-is

--- compile.go
+++ compile.go
@@ -1,8 +1,6 @@
 package shapes
 
 import (
-	"errors"
-
 	"github.com/johnny-morrice/shapes/asm"
 )
 
@@ -39,43 +37,54 @@ func (c *CompileVisitor) LeaveLoop(l *asm.LoopStmt) {
 	})
 }
 
-func (c *CompileVisitor) VisitAdd(add *asm.AddStmt) {
-	c.appendByteCode(Operation{
-		OpCode:  OP_ADD,
-		Operand: [2]Operand{Operand(add.Operand[0]), Operand(add.Operand[1])},
-	})
+func (c *CompileVisitor) VisitAdd(stmt *asm.AddStmt) {
+	c.appendByteCode(twoOp(OP_ADD, stmt.TwoOperandStmt))
 }
 
-func (c *CompileVisitor) VisitPop(p *asm.PopStmt) {
-	panic(errors.New("CompileVisitor.VisitPop not implemented"))
+func (c *CompileVisitor) VisitPop(stmt *asm.PopStmt) {
+	c.appendByteCode(twoOp(OP_POP, stmt.TwoOperandStmt))
 }
 
-func (c *CompileVisitor) VisitPush(p *asm.PushStmt) {
-	panic(errors.New("CompileVisitor.VisitPush not implemented"))
+func (c *CompileVisitor) VisitPush(stmt *asm.PushStmt) {
+	c.appendByteCode(twoOp(OP_PUSH, stmt.TwoOperandStmt))
 }
 
-func (c *CompileVisitor) VisitWrite(w *asm.WriteStmt) {
-	panic(errors.New("CompileVisitor.VisitWrite not implemented"))
+func (c *CompileVisitor) VisitWrite(stmt *asm.WriteStmt) {
+	c.appendByteCode(oneOp(OP_WRITE, stmt.OneOperandStmt))
 }
 
-func (c *CompileVisitor) VisitRead(r *asm.ReadStmt) {
-	panic(errors.New("CompileVisitor.VisitRead not implemented"))
+func (c *CompileVisitor) VisitRead(stmt *asm.ReadStmt) {
+	c.appendByteCode(oneOp(OP_READ, stmt.OneOperandStmt))
 }
 
-func (c *CompileVisitor) VisitSet(s *asm.SetStmt) {
-	panic(errors.New("CompileVisitor.VisitSet not implemented"))
+func (c *CompileVisitor) VisitSet(stmt *asm.SetStmt) {
+	c.appendByteCode(twoOp(OP_SET, stmt.TwoOperandStmt))
 }
 
-func (c *CompileVisitor) VisitCopy(s *asm.CopyStmt) {
-	panic(errors.New("CompileVisitor.VisitCopy not implemented"))
+func (c *CompileVisitor) VisitCopy(stmt *asm.CopyStmt) {
+	c.appendByteCode(twoOp(OP_COPY, stmt.TwoOperandStmt))
 }
 
-func (c *CompileVisitor) VisitSub(s *asm.SubStmt) {
-	panic(errors.New("CompileVisitor.VisitSub not implemented"))
+func (c *CompileVisitor) VisitSub(stmt *asm.SubStmt) {
+	c.appendByteCode(twoOp(OP_SUB, stmt.TwoOperandStmt))
 }
 
 func (c *CompileVisitor) appendByteCode(operations ...Operation) {
 	c.Process.ByteCode = append(c.Process.ByteCode, operations...)
+}
+
+func twoOp(opCode OpCode, stmt asm.TwoOperandStmt) Operation {
+	return Operation{
+		OpCode:  opCode,
+		Operand: [2]Operand{Operand(stmt.Operand[0]), Operand(stmt.Operand[1])},
+	}
+}
+
+func oneOp(opCode OpCode, stmt asm.OneOperandStmt) Operation {
+	return Operation{
+		OpCode:  opCode,
+		Operand: [2]Operand{Operand(stmt.Operand), 0},
+	}
 }
 
 const MAX_BYTECODE = 65535
